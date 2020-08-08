df1ee32dbe97d6a2fd36e4b21a98a91bcc58d54a
Will Leyton <wleylop22@gmail.com>
2020-08-08 17:57:34 -0500

    Update arrow version. Add Ref

--- src/main/kotlin/com/leysoft/adapter/out/persistence.kt
+++ src/main/kotlin/com/leysoft/adapter/out/persistence.kt
@@ -1,36 +1,56 @@
 package com.leysoft.adapter.out
 
 import arrow.Kind
+import arrow.core.None
 import arrow.core.Option
+import arrow.core.Some
+import arrow.fx.Ref
 import arrow.fx.typeclasses.Effect
 import com.leysoft.domain.Person
 import com.leysoft.domain.PersonRepository
 import java.lang.RuntimeException
 
 class InMemoryPersonRepository<F> private constructor(
-    private val Q: Effect<F>) : PersonRepository<F>, Effect<F> by Q {
+    private val Q: Effect<F>,
+    private val ref: Ref<F, Map<String, Person>>) : PersonRepository<F>, Effect<F> by Q {
 
-    private var store = mutableMapOf<String, Person>(
-        "12345" to Person(id = "12345", name = "Test")
-    )
+    override fun findById(id: String): Kind<F, Person> = ref.get()
+        .map { Option.fromNullable(it[id]) }
+        .flatMap {
+            when(it) {
+                is Some -> just(it.t)
+                else    -> raiseError(RuntimeException("Not found person: $id"))
+            }
+        }
 
-    override fun findById(id: String): Kind<F, Person> = Option.fromNullable(store[id]).fold(
-        { raiseError(RuntimeException("Not found person: $id")) },
-        { just(it) }
-    )
+    override fun findAll(): Kind<F, List<Person>> = ref.get()
+        .map { it.values.toList() }
 
-    override fun findAll(): Kind<F, List<Person>> = later { store.values.toList() }
+    override fun save(person: Person): Kind<F, Unit> = ref.get()
+        .map { Option.fromNullable(it[person.id]) }
+        .flatMap { result ->
+            when(result) {
+                is None -> ref.update { it.plus(Pair(person.id, person)) }
+                else    -> raiseError(RuntimeException("Not save person: $person"))
+            }
+        }
 
-    override fun save(person: Person): Kind<F, Unit> = Option.fromNullable(store.put(person.id, person))
-        .fold({ just(Unit) }, { just(Unit) })
-
-    override fun delete(person: Person): Kind<F, Boolean> = later { store.remove(person.id, person) }
-        .handleError { false }
+    override fun delete(person: Person): Kind<F, Boolean> = ref.get()
+        .map { Option.fromNullable(it[person.id]) }
+        .flatMap {
+            it.fold(
+                { ref.update { store -> store.minus(person.id) }.map { true } },
+                { just(false) }
+            )
+        }.handleError { false }
 
     companion object {
 
-        fun <F> build(Q: Effect<F>): PersonRepository<F> = InMemoryPersonRepository(Q)
+        fun <F> build(Q: Effect<F>, ref: Ref<F, Map<String, Person>>): PersonRepository<F> =
+            InMemoryPersonRepository(Q, ref)
 
-        fun <F> make(Q: Effect<F>): Kind<F, PersonRepository<F>> = Q.later { build(Q) }
+        fun <F> make(Q: Effect<F>, ref: Ref<F, Map<String, Person>>): Kind<F, PersonRepository<F>> =
+            Q.later { build(Q, ref) }
+
     }
 }
--- src/main/kotlin/com/leysoft/main.kt
+++ src/main/kotlin/com/leysoft/main.kt
@@ -1,14 +1,23 @@
 package com.leysoft
 
 import arrow.core.Either
+import arrow.fx.ForIO
 import arrow.fx.IO
+import arrow.fx.Ref
 import arrow.fx.extensions.io.effect.effect
 import arrow.fx.extensions.io.monad.flatMap
+import arrow.fx.extensions.io.monadDefer.monadDefer
+import arrow.fx.fix
+import arrow.fx.reactor.ForMonoK
 import arrow.fx.reactor.MonoK
 import arrow.fx.reactor.extensions.monok.effect.effect
+import arrow.fx.reactor.extensions.monok.monadDefer.monadDefer
 import arrow.fx.reactor.fix
+import arrow.fx.rx2.ForObservableK
 import arrow.fx.rx2.ObservableK
 import arrow.fx.rx2.extensions.observablek.effect.effect
+import arrow.fx.rx2.extensions.observablek.monadDefer.Ref
+import arrow.fx.rx2.extensions.observablek.monadDefer.monadDefer
 import arrow.fx.rx2.fix
 import com.leysoft.adapter.out.InMemoryPersonRepository
 import com.leysoft.application.DefaultPersonService
@@ -16,8 +25,14 @@ import com.leysoft.domain.Person
 
 fun main() {
 
+    val store = mapOf<String, Person>(
+        "12345" to Person(id = "12345", name = "Test")
+    )
+
     // Rx
-    val rxRepository = InMemoryPersonRepository.build(ObservableK.effect())
+    val refRx: Ref<ForObservableK, Map<String, Person>> = Ref(ObservableK.monadDefer(), store)
+        .fix().observable.blockingFirst()
+    val rxRepository = InMemoryPersonRepository.build(ObservableK.effect(), refRx)
     val rxService = DefaultPersonService.build(ObservableK.effect(), rxRepository)
     rxService.getAll().fix().observable.subscribe(::println)
     rxService.getById("12345").fix().observable.subscribe(::println)
@@ -25,18 +40,21 @@ fun main() {
     rxService.getAll().fix().observable.subscribe(::println)
 
     // Reactor Project
-    val reactorRepository = InMemoryPersonRepository.build(MonoK.effect())
+    val refReactor : Ref<ForMonoK, Map<String, Person>> = Ref(MonoK.monadDefer(), store)
+        .fix().mono.block()
+    val reactorRepository = InMemoryPersonRepository.build(MonoK.effect(), refReactor)
     val reactorService = DefaultPersonService.build(MonoK.effect(), reactorRepository)
     reactorService.getAll().fix().mono.subscribe(::println)
 
     // Arrow IO
-    InMemoryPersonRepository.make(IO.effect())
+    val refIO: IO<Ref<ForIO, Map<String, Person>>> = Ref(IO.monadDefer(), store).fix()
+    refIO.flatMap { InMemoryPersonRepository.make(IO.effect(), it) }
         .flatMap { DefaultPersonService.make(IO.effect(), it) }
         .flatMap { it.getAll() }
         .unsafeRunAsync {
             when (it) {
-                is Either.Right -> println(it.b)
-                else -> println("Error")
+                is Either.Right -> println("[IO] All: ${it.b}")
+                else            -> println("Error")
             }
         }
 }
