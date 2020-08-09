11c8901f83b6e6b90d6a0bae09976ba144d7fa48
Will Leyton <wleylop22@gmail.com>
2020-08-09 10:57:35 -0500

    Update

--- src/main/kotlin/com/leysoft/main.kt
+++ src/main/kotlin/com/leysoft/main.kt
@@ -1,11 +1,11 @@
 package com.leysoft
 
-import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
 import arrow.fx.ForIO
 import arrow.fx.IO
 import arrow.fx.Ref
 import arrow.fx.extensions.io.effect.effect
-import arrow.fx.extensions.io.monad.flatMap
 import arrow.fx.extensions.io.monadDefer.monadDefer
 import arrow.fx.fix
 import arrow.fx.reactor.ForMonoK
@@ -16,7 +16,6 @@ import arrow.fx.reactor.fix
 import arrow.fx.rx2.ForObservableK
 import arrow.fx.rx2.ObservableK
 import arrow.fx.rx2.extensions.observablek.effect.effect
-import arrow.fx.rx2.extensions.observablek.monadDefer.Ref
 import arrow.fx.rx2.extensions.observablek.monadDefer.monadDefer
 import arrow.fx.rx2.fix
 import com.leysoft.adapter.out.InMemoryPersonRepository
@@ -34,17 +33,29 @@ fun main() {
         .fix().observable.blockingFirst()
     val rxRepository = InMemoryPersonRepository.build(ObservableK.effect(), refRx)
     val rxService = DefaultPersonService.build(ObservableK.effect(), rxRepository)
-    rxService.getAll().fix().observable.subscribe(::println)
-    rxService.getById("12345").fix().observable.subscribe(::println)
+    rxService.getAll().fix().observable.subscribe(
+        { println("[Rx] All: $it") },
+        { println("[Rx] Error: $it") }
+    )
+    rxService.getById("12345").fix().observable.subscribe(
+        { println("[Rx] ById: $it") },
+        { println("[Rx] Error: $it") }
+    )
     rxService.create(Person(name = "New Person")).fix().observable.subscribe()
-    rxService.getAll().fix().observable.subscribe(::println)
+    rxService.getAll().fix().observable.subscribe(
+        { println("[Rx] All: $it") },
+        { println("[Rx] Error: $it") }
+    )
 
     // Reactor Project
     val refReactor : Ref<ForMonoK, Map<String, Person>> = Ref(MonoK.monadDefer(), store)
         .fix().mono.block()
     val reactorRepository = InMemoryPersonRepository.build(MonoK.effect(), refReactor)
     val reactorService = DefaultPersonService.build(MonoK.effect(), reactorRepository)
-    reactorService.getAll().fix().mono.subscribe(::println)
+    reactorService.getAll().fix().mono.subscribe(
+        { println("[Reactor] All: $it") },
+        { println("[Reactor] Error: $it") }
+    )
 
     // Arrow IO
     val refIO: IO<Ref<ForIO, Map<String, Person>>> = Ref(IO.monadDefer(), store).fix()
@@ -53,8 +64,8 @@ fun main() {
         .flatMap { it.getAll() }
         .unsafeRunAsync {
             when (it) {
-                is Either.Right -> println("[IO] All: ${it.b}")
-                else            -> println("Error")
+                is Right -> println("[IO] All: ${it.b}")
+                is Left  -> println("[IO] Error: ${it.a}")
             }
         }
 }
